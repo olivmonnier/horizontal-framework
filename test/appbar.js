c2fb4f13cee018db43ace5772eb44d3a551e8a04
Olivier Monnier <omonnier.dev@gmail.com>
2017-11-23 14:44:45 +0100

    add test

--- test/appbar.js
+++ test/appbar.js
@@ -1,11 +1,18 @@
-const describe = require('mocha').describe,
-  it = require('mocha').it,
-  expect = require('chai').expect;
+import AppBar from '../src/js/components/appbar';
+const describe = require('mocha').describe;
+const it = require('mocha').it;
+const expect = require('chai').expect;
 
 const $ = require('./fixtures/appbar');
 
 describe('Appbar', () => {
   it('should appbar exists', () => {
     expect($('.appbar').length).equal(1)
+  })
+  it('should \'in\' className after click on toggle button', () => {
+    new AppBar($('.appbar')[0])
+
+    $('.appbar button').click()
+    expect($('.appbar').hasClass('in')).equal(true)
   })
 })
